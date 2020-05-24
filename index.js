74c85c0fc791be5a986930c1aabebcdc65d98066
dy <df.creative@gmail.com>
2020-05-24 19:55:30 -0400

    Codegolf

--- index.js
+++ index.js
@@ -5,22 +5,39 @@ export default (el, types) => {
       k === Symbol.asyncIterator ? asyncIterable(el, p) :
       k in el ? el[k] :
       a[k] && typed(a[k].value, types && types[k]),
-    set: (_, k, v) => set(el, p, k, v, types && types[k]),
+
+    set: (_, k, v) => {
+      el[k] = typed(v, types && types[k])
+
+      if (el.setAttribute) {
+        if (v === false || v == null) el.removeAttribute(k)
+        else el.setAttribute(k,
+          v === true ? '' :
+          typeof v === 'number' || typeof v === 'string' ? v :
+          k === 'class' && Array.isArray(v) ? v.filter(Boolean).join(' ') :
+          k === 'style' && v.constructor === Object ?
+            (k=v,v=Object.values(v),Object.keys(k).map((k,i) => `${k}: ${v[i]};`).join(' ')) :
+          ''
+        )
+        dispatch(el, p)
+      }
+
+      return true
+    },
 
     // spread https://github.com/tc39/proposal-object-rest-spread/issues/69#issuecomment-633232470
-    getOwnPropertyDescriptor,
+    getOwnPropertyDescriptor: () => desc,
 
     // joined props from element keys and real attributes
     ownKeys: _ => Array.from(
-      new Set(
-        Object.keys(el).filter(k => el[k] !== p)
-        .concat(Object.getOwnPropertyNames(_).filter(v => isNaN(Number(v))))
-      )
+      new Set([...Object.keys(el), ...Object.getOwnPropertyNames(_)].filter(k => el[k] !== p && isNaN(Number(k))))
     )
   });
 
   return p
 }
+
+const desc = { enumerable: true, configurable: true }
 
 // auto-parse pkg in 2 lines (no object/array detection)
 // Number(n) is fast: https://jsperf.com/number-vs-plus-vs-toint-vs-tofloat/35
@@ -29,64 +46,36 @@ const typed = ( v, t, n ) => (
   v == '' && !t || t === Boolean ? true : t ? t(v) : isNaN(n=Number(v)) ? v : n
 )
 
-const set = (el, props, k, v, t) => {
-  el[k] = typed(v, t)
-
-  if (el.setAttribute) {
-    if (v === false || v == null) el.removeAttribute(k)
-    else if (v === true) el.setAttribute(k, '')
-    else if (typeof v === 'number' || typeof v === 'string') el.setAttribute(k, v)
-    // class=[a, b, ...c]
-    else if (k === 'class' && Array.isArray(v)) el.setAttribute(k, v.filter(Boolean).join(' '))
-    // onclick={} - skip
-    else if (typeof v === 'function') {}
-    // style={}
-    else if (k === 'style' && v.constructor === Object)
-      el.setAttribute(k,(k=v,v=Object.values(v),Object.keys(k).map((k,i) => `${k}: ${v[i]};`).join(' ')))
-    else return true
-
-    dispatch(el, props)
-  }
-
-  return true
-}
-
-const desc = { enumerable: true, configurable: true }, getOwnPropertyDescriptor = () => desc
-
 const observed = new WeakMap
-const observable = (el, props) => {
-  return () => ({
-    subscribe(observer) {
-      // MO does not prevent garbage collecting removed node https://dom.spec.whatwg.org/#garbage-collection
-      // FIXME: not sure will the MO itself be collected
-      if (!observed.has(el)) {
-        let mo = new MutationObserver(() => dispatch(el, props))
-        observed.set(el, mo)
-        mo.observe(el,{ attributes:true })
-      }
+const observable = (el, props) => () => ({
+  subscribe(next) {
+    // MO does not prevent garbage collecting removed node https://dom.spec.whatwg.org/#garbage-collection
+    // FIXME: not sure will the MO itself be collected
+    if (!observed.has(el)) {
+      let mo = new MutationObserver(() => dispatch(el, props))
+      observed.set(el, mo)
+      mo.observe(el,{ attributes:true })
+    }
 
-      const next = observer.next || observer, handler = e => next(e.detail.props)
+    (next = next.next || next)(props)
 
-      // immediate value
-      next(props)
+    const handler = e => next(e.detail.props),
+      unsubscribe = () => el.removeEventListener('props', handler)
 
-      el.addEventListener('props', handler)
-      const unsubscribe = () => el.removeEventListener('props', handler)
-      return unsubscribe.unsubscribe = unsubscribe
-    },
-    [Symbol.observable]() { return this }
-  })
-}
+    el.addEventListener('props', handler)
 
-const asyncIterable = (el, props) => {
-  return async function*() {
-    let resolve, buf = [], p = new Promise(r => resolve = r),
-      unsubscribe = props[Symbol.observable]().subscribe(v => ( buf.push(v), resolve(), p = new Promise(r => resolve = r) ))
+    return unsubscribe.unsubscribe = unsubscribe
+  },
+  [Symbol.observable]() { return this }
+})
 
-    try { while (1) yield* buf.splice(0), await p }
-    catch {}
-    finally { unsubscribe() }
-  }
+const asyncIterable = (el, props) => async function*() {
+  let resolve, buf = [], p = new Promise(r => resolve = r),
+    unsubscribe = props[Symbol.observable]().subscribe(v => ( buf.push(v), resolve(), p = new Promise(r => resolve = r) ))
+
+  try { while (1) yield* buf.splice(0), await p }
+  catch {}
+  finally { unsubscribe() }
 }
 
 const dispatch = (el, props) => el.dispatchEvent(new CustomEvent('props', { detail: { props }}))
