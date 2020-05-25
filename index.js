8a84e16a4fbe8192056c1127a1d382cacb974d74
dy <df.creative@gmail.com>
2020-05-24 20:25:47 -0400

    Codegulf 0o0

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ export default (el, types) => {
     },
 
     // spread https://github.com/tc39/proposal-object-rest-spread/issues/69#issuecomment-633232470
-    getOwnPropertyDescriptor: () => desc,
+    getOwnPropertyDescriptor: _ => desc,
 
     // joined props from element keys and real attributes
     ownKeys: _ => Array.from(
-      new Set([...Object.keys(el), ...Object.getOwnPropertyNames(_)].filter(k => el[k] !== p && isNaN(Number(k))))
+      new Set([...Object.keys(el), ...Object.getOwnPropertyNames(a)].filter(k => el[k] !== p && isNaN(+k)))
     )
   });
 
@@ -39,19 +39,18 @@ const desc = { enumerable: true, configurable: true }
 
 // auto-parse pkg in 2 lines (no object/array detection)
 // Number(n) is fast: https://jsperf.com/number-vs-plus-vs-toint-vs-tofloat/35
-const typed = ( v, t, n ) => (
+const typed = ( v, t ) => (
   t = t === Object || t === Array ? JSON.parse : t,
-  v == '' && !t || t === Boolean ? true : t ? t(v) : isNaN(n=Number(v)) ? v : n
+  v == '' && t !== String ? true : t ? t(v) : isNaN(t=+v) ? v : t
 )
 
 const observable = (el, props) => () => ({
-  subscribe(next=next.next||next) {
+  subscribe(next) {
     // MO does not prevent garbage collecting removed node https://dom.spec.whatwg.org/#garbage-collection
-    const enext = e => next(e.detail.props),
-      mo = new MutationObserver(() => dispatch(el, props)),
-      unsub = () => (el.removeEventListener('props', enext), mo.disconnect())
+    const mo = new MutationObserver(() => dispatch(el, props)),
+      unsub = () => (el.removeEventListener('props', next), mo.disconnect())
 
-    mo.observe(el,{ attributes:true }), next(props), el.addEventListener('props', enext)
+    mo.observe(el,{ attributes:true }), (next=(next.next||next).bind(null,props))(), el.addEventListener('props', next)
     return unsub.unsubscribe = unsub
   },
   [Symbol.observable]() { return this }
