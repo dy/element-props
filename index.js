43ea972620add820cc5842bf9cd7378e51849446
dy <df.creative@gmail.com>
2020-05-24 20:12:45 -0400

    Codegulf more

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ export default (el, types) => {
   let a = el.attributes, p = new Proxy(a, {
     get: (_,k) =>
       k === Symbol.observable ? observable(el, p) :
-      k === Symbol.asyncIterator ? asyncIterable(el, p) :
+      k === Symbol.asyncIterator ? asyncIterator(el, p) :
       k in el ? el[k] :
       a[k] && typed(a[k].value, types && types[k]),
 
     set: (_, k, v) => {
       el[k] = typed(v, types && types[k])
 
-      if (el.setAttribute) {
-        if (v === false || v == null) el.removeAttribute(k)
-        else el.setAttribute(k,
-          v === true ? '' :
-          typeof v === 'number' || typeof v === 'string' ? v :
-          k === 'class' && Array.isArray(v) ? v.filter(Boolean).join(' ') :
-          k === 'style' && v.constructor === Object ?
-            (k=v,v=Object.values(v),Object.keys(k).map((k,i) => `${k}: ${v[i]};`).join(' ')) :
-          ''
-        )
-        dispatch(el, p)
-      }
+      if (v === false || v == null) el.removeAttribute(k)
+      else el.setAttribute(k,
+        v === true ? '' :
+        typeof v === 'number' || typeof v === 'string' ? v :
+        k === 'class' && Array.isArray(v) ? v.filter(Boolean).join(' ') :
+        k === 'style' && v.constructor === Object ?
+          (k=v,v=Object.values(v),Object.keys(k).map((k,i) => `${k}: ${v[i]};`).join(' ')) :
+        ''
+      )
+      dispatch(el, p)
 
       return true
     },
@@ -46,36 +44,26 @@ const typed = ( v, t, n ) => (
   v == '' && !t || t === Boolean ? true : t ? t(v) : isNaN(n=Number(v)) ? v : n
 )
 
-const observed = new WeakMap
 const observable = (el, props) => () => ({
-  subscribe(next) {
+  subscribe(next=next.next||next) {
     // MO does not prevent garbage collecting removed node https://dom.spec.whatwg.org/#garbage-collection
-    // FIXME: not sure will the MO itself be collected
-    if (!observed.has(el)) {
-      let mo = new MutationObserver(() => dispatch(el, props))
-      observed.set(el, mo)
-      mo.observe(el,{ attributes:true })
-    }
+    const enext = e => next(e.detail.props),
+      mo = new MutationObserver(() => dispatch(el, props)),
+      unsub = () => (el.removeEventListener('props', enext), mo.disconnect())
 
-    (next = next.next || next)(props)
-
-    const handler = e => next(e.detail.props),
-      unsubscribe = () => el.removeEventListener('props', handler)
-
-    el.addEventListener('props', handler)
-
-    return unsubscribe.unsubscribe = unsubscribe
+    mo.observe(el,{ attributes:true }), next(props), el.addEventListener('props', enext)
+    return unsub.unsubscribe = unsub
   },
   [Symbol.observable]() { return this }
 })
 
-const asyncIterable = (el, props) => async function*() {
+const asyncIterator = (el, props) => async function*() {
   let resolve, buf = [], p = new Promise(r => resolve = r),
-    unsubscribe = props[Symbol.observable]().subscribe(v => ( buf.push(v), resolve(), p = new Promise(r => resolve = r) ))
+    unsub = props[Symbol.observable]().subscribe(v => ( buf.push(v), resolve(), p = new Promise(r => resolve = r) ))
 
   try { while (1) yield* buf.splice(0), await p }
   catch {}
-  finally { unsubscribe() }
+  finally { unsub() }
 }
 
 const dispatch = (el, props) => el.dispatchEvent(new CustomEvent('props', { detail: { props }}))
